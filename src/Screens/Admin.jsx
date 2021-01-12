7c6d408bc21e4ef081d5d2068af71203ad0c466f
frangild <32389531+frangild@users.noreply.github.com>
2021-01-12 00:54:25 +0000

    Add Nominations to Admin

--- src/Screens/Admin.jsx
+++ src/Screens/Admin.jsx
@@ -1,30 +1,33 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Component } from "react";
 import authSvg from "../assets/update.svg";
 import { ToastContainer, toast } from "react-toastify";
 import { Link, Redirect } from "react-router-dom";
 import axios from "axios";
 import { updateUser, isAuth, getCookie, signout } from "../helpers/auth";
+import { isEmpty } from "../helpers/basic";
 
-const Admin = ({ history }) => {
-  const [formData, setFormData] = useState({
-    first_name: "",
-    last_name: "",
-    pref_first_name: "",
-    student_number: "",
-    textChange: "Update",
-    role: "",
-  });
+class Admin extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      token: "",
+      first_name: "",
+      last_name: "",
+      pref_first_name: "",
+      student_number: "",
+      textChange: "Update",
+      role: "",
+      nominees: [],
+      nominations: [],
+      loaded: false,
+    };
+  }
 
-  useEffect(() => {
-    loadProfile();
-  }, []);
-
-  const loadProfile = () => {
-    const token = getCookie("token");
-    axios
+  loadProfile = async () => {
+    await axios
       .get(`${process.env.REACT_APP_API_URL}/user/${isAuth()._id}`, {
         headers: {
-          Authorization: `Bearer ${token}`,
+          Authorization: `Bearer ${this.state.token}`,
         },
       })
       .then((res) => {
@@ -35,8 +38,7 @@ const Admin = ({ history }) => {
           pref_first_name,
           student_number,
         } = res.data;
-        setFormData({
-          ...formData,
+        this.setState({
           role,
           first_name,
           last_name,
@@ -48,74 +50,187 @@ const Admin = ({ history }) => {
         toast.error(`Error To Your Information ${err.response.statusText}`);
         if (err.response.status === 401) {
           signout(() => {
-            history.push("/login");
+            this.props.history.push("/login");
           });
         }
       });
   };
 
-  const {
-    first_name,
-    last_name,
-    pref_first_name,
-    student_number,
-    textChange,
-    role,
-  } = formData;
+  loadNominees = async () => {
+    await axios
+      .get(`${process.env.REACT_APP_API_URL}/nomination/nominees`)
+      .then((res) => {
+        this.setState({ nominees: res.data, loaded: true });
+      })
+      .catch((err) => {
+        toast.error(`Could not retrieve nominees data.`);
+        this.setState({ loaded: true });
+      });
+  };
 
-  return (
-    <div className="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
-      <ToastContainer />
-      <div className="max-w-screen-xl m-0 sm:m-20 bg-white shadow sm:rounded-lg flex justify-center flex-1">
-        <div className="lg:w-1/2 xl:w-8/12 p-6 sm:p-12">
-          <div className="mt-12 flex flex-col items-center">
-            <Link to="/" className="text-xl xl:text-xl text-center ">
-              {"<"} Home
-            </Link>
-            <div className="w-full flex-1 mt-8 text-indigo-500">
-              <div className="mx-auto max-w-xs relative text-center text-blue-700">
-                If you encounter any issues with the system, please email{" "}
-                <strong>welfareiadt@gmail.com</strong>.
-              </div>
-              <div className="my-12 border-b text-center">
-                <div className="leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2">
-                  Select destination
+  loadNominations = async () => {
+    const { token } = this.state;
+
+    await axios
+      .get(`${process.env.REACT_APP_API_URL}/nomination/all`)
+      .then((res) => {
+        this.setState({ nominations: res.data, loaded: true });
+      })
+      .catch((err) => {
+        toast.error(err.response.data.message);
+        this.setState({ loaded: true });
+      });
+  };
+
+  componentDidMount() {
+    this.setState({ token: getCookie("token") }, async () => {
+      await this.loadProfile();
+      await this.loadNominees();
+      await this.loadNominations();
+    });
+  }
+
+  render() {
+    const {
+      first_name,
+      last_name,
+      pref_first_name,
+      student_number,
+      textChange,
+      role,
+      loaded,
+    } = this.state;
+
+    if (!loaded) {
+      return "Loading...";
+    }
+
+    const { nominees, nominations } = this.state;
+    console.log(nominees[0].user);
+    console.log(nominations);
+
+    return (
+      <div className="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
+        <ToastContainer />
+        <div className="max-w-screen-xl m-0 sm:m-20 bg-white shadow sm:rounded-lg flex justify-center flex-1">
+          <div className="lg:w-1/2 xl:w-8/12 p-6 sm:p-12">
+            <div className="mt-12 flex flex-col items-center">
+              <Link to="/" className="text-xl xl:text-xl text-center ">
+                {"<"} Home
+              </Link>
+              <div className="w-full flex-1 mt-8 text-indigo-500">
+                <div className="mx-auto max-w-xs relative text-center text-blue-700">
+                  If you encounter any issues with the system, please email{" "}
+                  <strong>welfareiadt@gmail.com</strong>.
                 </div>
-              </div>
-              <div className="mx-auto max-w-xs relative ">
-                <Link
-                  to="/dashboard"
-                  className="mt-5 tracking-wide font-semibold bg-blue-500 text-gray-100 w-full py-4 rounded-lg hover:bg-green-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none"
-                >
-                  <i className="fas fa-sign-in-alt  w-6  -ml-2" />
-                  <span className="ml-3">Dashbaord</span>
-                </Link>
-                <Link
-                  to="/nominations"
-                  className="mt-5 tracking-wide font-semibold bg-orange-500 text-gray-100 w-full py-4 rounded-lg hover:bg-green-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none"
-                >
-                  <i className="fas fa-sign-in-alt  w-6  -ml-2" />
-                  <span className="ml-3">Nominations</span>
-                </Link>
-                <button
-                  onClick={() => {
-                    signout(() => {
-                      toast.success("Signed out successfully");
-                      history.push("/");
-                    });
-                  }}
-                  className="mt-5 tracking-wide font-semibold bg-pink-500 text-gray-100 w-full py-4 rounded-lg hover:bg-pink-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none"
-                >
-                  <i className="fas fa-sign-out-alt  w-6  -ml-2" />
-                  <span className="ml-3">Sign Out</span>
-                </button>
+                <div className="my-12 border-b text-center">
+                  <div className="leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2">
+                    Nominations
+                  </div>
+                </div>
+                <div className="mx-auto px-auto max-w-xl relative text-center">
+                  {!isEmpty(nominees)
+                    ? nominees.map((n, i) => {
+                        return (
+                          <React.Fragment key={i}>
+                            <table className="shadow-lg mb-4 mx-auto">
+                              <tr>
+                                <th
+                                  className="bg-blue-100 border text-center px-8 py-4"
+                                  colSpan="3"
+                                >
+                                  {n.user.pref_first_name} {n.user.last_name} (
+                                  {n.role}) - Nominations
+                                </th>
+                              </tr>
+                              <tr>
+                                <th className="border text-left px-8 py-2"></th>
+                                <th className="border text-left px-8 py-2">
+                                  Name
+                                </th>
+                                <th className="border text-left px-8 py-2">
+                                  Student Number
+                                </th>
+                              </tr>
+                              {!isEmpty(
+                                nominations.filter(
+                                  (nom) => nom.nominee._id == n._id
+                                )
+                              ) ? (
+                                nominations
+                                  .filter((nom) => nom.nominee._id == n._id)
+                                  .map((nom, i) => {
+                                    return (
+                                      <tr key={i}>
+                                        <td className="border text-left px-8 py-2">
+                                          {i + 1}
+                                        </td>
+                                        <td className="border text-left px-8 py-2">
+                                          {nom.signature.pref_first_name}{" "}
+                                          {nom.signature.last_name}
+                                        </td>
+                                        <td className="border text-left px-8 py-2">
+                                          {nom.signature.student_number}
+                                        </td>
+                                      </tr>
+                                    );
+                                  })
+                              ) : (
+                                <tr>
+                                  <td
+                                    className="border text-left px-8 py-2"
+                                    colSpan="3"
+                                  >
+                                    This nominee does not have any signatures.
+                                  </td>
+                                </tr>
+                              )}
+                            </table>
+                          </React.Fragment>
+                        );
+                      })
+                    : "There are no nominees."}
+                </div>
+                <div className="my-12 border-b text-center">
+                  <div className="leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2">
+                    Select destination
+                  </div>
+                </div>
+                <div className="mx-auto max-w-xs relative ">
+                  <Link
+                    to="/dashboard"
+                    className="mt-5 tracking-wide font-semibold bg-blue-500 text-gray-100 w-full py-4 rounded-lg hover:bg-green-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none"
+                  >
+                    <i className="fas fa-sign-in-alt  w-6  -ml-2" />
+                    <span className="ml-3">Dashboard</span>
+                  </Link>
+                  <Link
+                    to="/nominations"
+                    className="mt-5 tracking-wide font-semibold bg-orange-500 text-gray-100 w-full py-4 rounded-lg hover:bg-green-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none"
+                  >
+                    <i className="fas fa-sign-in-alt  w-6  -ml-2" />
+                    <span className="ml-3">Nominations</span>
+                  </Link>
+                  <button
+                    onClick={() => {
+                      signout(() => {
+                        toast.success("Signed out successfully");
+                        this.props.history.push("/");
+                      });
+                    }}
+                    className="mt-5 tracking-wide font-semibold bg-pink-500 text-gray-100 w-full py-4 rounded-lg hover:bg-pink-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none"
+                  >
+                    <i className="fas fa-sign-out-alt  w-6  -ml-2" />
+                    <span className="ml-3">Sign Out</span>
+                  </button>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Admin;
