3b829cd025794b4383184b46ceaef79758bdcd4c
frangild <32389531+frangild@users.noreply.github.com>
2021-03-07 11:24:45 +0000

    old

--- src/Screens/Admin.jsx
+++ src/Screens/Admin.jsx
@@ -20,6 +20,9 @@ class Admin extends Component {
       role: "",
       nominees: [],
       nominations: [],
+      election: {},
+      candidates: [],
+      referenda: [],
       loaded: false,
     };
   }
@@ -61,11 +64,10 @@ class Admin extends Component {
     await axios
       .get(`${process.env.REACT_APP_API_URL}/nomination/nominees`)
       .then((res) => {
-        this.setState({ nominees: res.data, loaded: true });
+        this.setState({ nominees: res.data });
       })
       .catch((err) => {
         toast.error(`Could not retrieve nominees data.`);
-        this.setState({ loaded: true });
       });
   };
 
@@ -75,7 +77,57 @@ class Admin extends Component {
     await axios
       .get(`${process.env.REACT_APP_API_URL}/nomination/all`)
       .then((res) => {
-        this.setState({ nominations: res.data, loaded: true });
+        this.setState({ nominations: res.data });
+      })
+      .catch((err) => {
+        toast.error(err.response.data.message);
+      });
+  };
+
+  loadElection = async () => {
+    await axios
+      .post(`${process.env.REACT_APP_API_URL}/election/year`, { year: "2021" })
+      .then((res) => {
+        this.setState({ election: res.data[0] });
+      })
+      .catch((err) => {
+        toast.error(`Could not retrieve election data.`);
+      });
+  };
+
+  loadCandidates = async () => {
+    await axios
+      .get(
+        `${process.env.REACT_APP_API_URL}/election/${this.state.election._id}/candidates`
+      )
+      .then((res) => {
+        this.setState({ candidates: res.data });
+      })
+      .catch((err) => {
+        toast.error(err.response.data.message);
+      });
+  };
+
+  loadReferenda = async () => {
+    await axios
+      .get(
+        `${process.env.REACT_APP_API_URL}/election/${this.state.election._id}/referenda`
+      )
+      .then((res) => {
+        this.setState({ referenda: res.data });
+      })
+      .catch((err) => {
+        toast.error(err.response.data.message);
+      });
+  };
+
+  loadBallots = async () => {
+    await axios
+      .get(
+        `${process.env.REACT_APP_API_URL}/election/${this.state.election._id}/ballots`
+      )
+      .then((res) => {
+        this.setState({ ballots: res.data, loaded: true });
       })
       .catch((err) => {
         toast.error(err.response.data.message);
@@ -88,6 +140,10 @@ class Admin extends Component {
       await this.loadProfile();
       await this.loadNominees();
       await this.loadNominations();
+      await this.loadElection();
+      await this.loadCandidates();
+      await this.loadReferenda();
+      await this.loadBallots();
     });
   }
 
@@ -106,9 +162,19 @@ class Admin extends Component {
       return "Loading...";
     }
 
-    const { nominees, nominations } = this.state;
-    console.log(nominees[0].user);
-    console.log(nominations);
+    const {
+      nominees,
+      nominations,
+      election,
+      candidates,
+      referenda,
+      ballots,
+    } = this.state;
+    console.log("election", election);
+    console.log("candidates", candidates);
+    console.log("referenda", referenda);
+    console.log("ballots", ballots);
+    const roles = ["mature", "socs"];
 
     return (
       <div className="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
@@ -125,6 +191,144 @@ class Admin extends Component {
                   <strong>welfareiadt@gmail.com</strong>.
                 </div>
                 <div className="my-12 border-b text-center">
+                  <div className="leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2">
+                    Election Results
+                  </div>
+                </div>
+                <div className="mx-auto px-auto max-w-xl relative text-center">
+                  <table className="shadow-lg mb-4 mx-auto">
+                    <thead>
+                      <tr>
+                        <th className="border text-left px-8 py-2">Role</th>
+                        <th className="border text-left px-8 py-2">
+                          Student Number
+                        </th>
+                        <th className="border text-left px-8 py-2">Name</th>
+                        <th className="border text-left px-8 py-2">Votes</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {candidates.map((c, i) => {
+                        let count = 0;
+                        return (
+                          <tr key={i}>
+                            <td className="border text-left px-8 py-2">
+                              {c.role}
+                            </td>
+                            <td className="border text-left px-8 py-2">
+                              {c.user.student_number}
+                            </td>
+                            <td className="border text-left px-8 py-2">
+                              {c.user.pref_first_name} {c.user.last_name}
+                            </td>
+                            <td className="border text-left px-8 py-2">
+                              {ballots.map((b, i) => {
+                                return b.candidateVotes.map((v, i) => {
+                                  if (c._id == v.candidate) {
+                                    count++;
+                                  }
+                                });
+                              })}
+                              {count}
+                            </td>
+                          </tr>
+                        );
+                      })}
+
+                      {/* 
+                    
+                    HARDCODED - REMOVE !!!!!! 
+                    
+                    */}
+                      {roles.map((r, i) => {
+                        let count = 0;
+                        return (
+                          <React.Fragment key={i}>
+                            <tr>
+                              <td className="border text-left px-8 py-2">
+                                {r}
+                              </td>
+                              <td
+                                className="border text-left px-8 py-2"
+                                colSpan={2}
+                              >
+                                Reopen Nominations
+                              </td>
+                              <td className="border text-left px-8 py-2">
+                                {ballots.map((b, i) => {
+                                  return b.reopenNominations.map((v, i) => {
+                                    if (v && v.role == r) {
+                                      count++;
+                                    }
+                                  });
+                                })}
+                                {count}
+                              </td>
+                            </tr>
+                          </React.Fragment>
+                        );
+                      })}
+                    </tbody>
+                  </table>
+                  <table className="shadow-lg mb-4 mx-auto">
+                    <thead>
+                      <tr>
+                        <th
+                          className="border text-left px-8 py-2 text-center"
+                          colSpan={3}
+                        >
+                          Referenda
+                        </th>
+                      </tr>
+                      <tr>
+                        <th className="border text-left px-8 py-2">Title</th>
+                        <th className="border text-left px-8 py-2">
+                          Votes For
+                        </th>
+                        <th className="border text-left px-8 py-2">
+                          Votes Against
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {referenda.map((r, i) => {
+                        let countFor = 0;
+                        let countAgainst = 0;
+                        return (
+                          <tr key={i}>
+                            <td className="border text-left px-8 py-2">
+                              {r.title}
+                            </td>
+                            <td className="border text-left px-8 py-2">
+                              {ballots.map((b, i) => {
+                                b.referenda.map((ref, i) => {
+                                  if (r._id == ref.referendum && ref.votedFor) {
+                                    countFor++;
+                                  }
+                                });
+                              })}
+                              {countFor}
+                            </td>
+                            <td className="border text-left px-8 py-2">
+                              {ballots.map((b, i) => {
+                                b.referenda.map((ref, i) => {
+                                  if (
+                                    r._id == ref.referendum &&
+                                    !ref.votedFor
+                                  ) {
+                                    countAgainst++;
+                                  }
+                                });
+                              })}
+                              {countAgainst}
+                            </td>
+                          </tr>
+                        );
+                      })}
+                    </tbody>
+                  </table>
+                </div>
+                {/* <div className="my-12 border-b text-center">
                   <div className="leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2">
                     Nominations
                   </div>
@@ -192,7 +396,7 @@ class Admin extends Component {
                         );
                       })
                     : "There are no nominees."}
-                </div>
+                </div> */}
                 <div className="my-12 border-b text-center">
                   <div className="leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2">
                     Select destination
--- src/Screens/elections/Elections.jsx
+++ src/Screens/elections/Elections.jsx
@@ -15,7 +15,13 @@ class Elections extends Component {
       last_name: "",
       pref_first_name: "",
       student_number: "",
-      role: "",
+      election: {},
+      candidates: [],
+      referenda: [],
+      socs: "",
+      mature: "",
+      referendum: "",
+      voted: false,
       loaded: false,
     };
   }
@@ -23,53 +29,132 @@ class Elections extends Component {
   componentDidMount() {
     this.setState({ token: getCookie("token") }, async () => {
       await this.loadProfile();
-      // await this.loadNominations();
-      // await this.loadSignatures();
-      // this.loadNominees();
+      await this.loadElection();
+      await this.loadCandidates();
+      await this.loadReferenda();
+      this.loadVotes();
     });
   }
 
-  // loadNominees = async () => {
-  //   await axios
-  //     .get(`${process.env.REACT_APP_API_URL}/nomination/nominees`)
+  // Handle change from inputs
+  handleChange = (text) => (e) => {
+    this.setState({ [text]: e.target.value }, () => {
+      // console.log(this.state[text]);
+    });
+  };
+
+  // Submit data to backend
+  // handleSubmit = (e) => {
+  //   e.preventDefault();
+
+  //   const { token, socs, mature, referendum, voted } = this.state;
+  //   const election = this.state.election._id;
+
+  //   if (voted) {
+  //     return toast.error("You have already voted in this election!");
+  //   }
+
+  //   if (!socs) {
+  //     return toast.error(
+  //       "You must cast a vote for the Clubs & Societies role."
+  //     );
+  //   }
+
+  //   if (!mature) {
+  //     return toast.error("You must cast a vote for the Mature Student role.");
+  //   }
+
+  //   if (!referendum) {
+  //     return toast.error("You must cast a vote for the referendum.");
+  //   }
+
+  //   const ballot = {
+  //     token,
+  //     election,
+  //     candidateVotes: [],
+  //     reopenNominations: [],
+  //     referenda: [
+  //       { referendum: this.state.referenda[0]._id, votedFor: referendum },
+  //     ],
+  //   };
+
+  //   if (socs === "reopen") {
+  //     ballot.reopenNominations.push({ role: "socs" });
+  //   } else {
+  //     ballot.candidateVotes.push({ candidate: socs, role: "socs" });
+  //   }
+
+  //   if (mature === "reopen") {
+  //     ballot.reopenNominations.push({ role: "mature" });
+  //   } else {
+  //     ballot.candidateVotes.push({ candidate: mature, role: "mature" });
+  //   }
+
+  //   axios
+  //     .post(`${process.env.REACT_APP_API_URL}/election/vote`, ballot)
   //     .then((res) => {
-  //       this.setState({ nominees: res.data, loaded: true });
+  //       toast.success(res.data.message);
   //     })
   //     .catch((err) => {
-  //       toast.error(`Could not retrieve nominees data.`);
-  //       this.setState({ loaded: true });
+  //       toast.error(err.response.data.error);
   //     });
   // };
 
-  // loadNominations = async () => {
-  //   const { token } = this.state;
+  loadElection = async () => {
+    await axios
+      .post(`${process.env.REACT_APP_API_URL}/election/year`, { year: "2021" })
+      .then((res) => {
+        this.setState({ election: res.data[0] });
+      })
+      .catch((err) => {
+        toast.error(`Could not retrieve election data.`);
+      });
+  };
 
-  //   await axios
-  //     .post(`${process.env.REACT_APP_API_URL}/nomination/signee/mine`, {
-  //       token,
-  //     })
-  //     .then((res) => {
-  //       this.setState({ nominations: res.data });
-  //     })
-  //     .catch((err) => {
-  //       toast.error(err.response.data.message);
-  //     });
-  // };
+  loadCandidates = async () => {
+    await axios
+      .get(
+        `${process.env.REACT_APP_API_URL}/election/${this.state.election._id}/candidates`
+      )
+      .then((res) => {
+        this.setState({ candidates: res.data });
+      })
+      .catch((err) => {
+        toast.error(err.response.data.message);
+      });
+  };
 
-  // loadSignatures = async () => {
-  //   const { token } = this.state;
+  loadReferenda = async () => {
+    await axios
+      .get(
+        `${process.env.REACT_APP_API_URL}/election/${this.state.election._id}/referenda`
+      )
+      .then((res) => {
+        this.setState({ referenda: res.data });
+      })
+      .catch((err) => {
+        toast.error(err.response.data.message);
+      });
+  };
 
-  //   await axios
-  //     .post(`${process.env.REACT_APP_API_URL}/nomination/nominee/mine`, {
-  //       token,
-  //     })
-  //     .then((res) => {
-  //       this.setState({ signatures: res.data });
-  //     })
-  //     .catch((err) => {
-  //       toast.error(err.response.data.message);
-  //     });
-  // };
+  loadVotes = async () => {
+    await axios
+      .post(`${process.env.REACT_APP_API_URL}/election/votes/mine`, {
+        token: this.state.token,
+        election: this.state.election._id,
+      })
+      .then((res) => {
+        if (!isEmpty(res.data)) {
+          this.setState({ voted: true, loaded: true });
+        } else {
+          this.setState({ voted: false, loaded: true });
+        }
+      })
+      .catch((err) => {
+        toast.error(err.response.data.message);
+        this.setState({ loaded: true });
+      });
+  };
 
   loadProfile = async () => {
     const token = getCookie("token");
@@ -93,7 +178,6 @@ class Elections extends Component {
           last_name,
           pref_first_name,
           student_number,
-          loaded: true,
         });
       })
       .catch((err) => {
@@ -112,11 +196,10 @@ class Elections extends Component {
       last_name,
       pref_first_name,
       student_number,
-      textChange,
       role,
-      nominees,
-      nominations,
-      signatures,
+      election,
+      candidates,
+      referenda,
       loaded,
     } = this.state;
 
@@ -124,13 +207,11 @@ class Elections extends Component {
       return "Loading...";
     }
 
-    // let nominated = false;
-    // nominees.map((n) => {
-    //   if (n.user.student_number == this.state.student_number) {
-    //     return (nominated = true);
-    //   }
-    // });
-    // console.log("nominated: " + nominated);
+    // __________________________________________
+    //
+    // HARD CODED MANIPULATION, GET RID OF IT!!!
+    // __________________________________________
+    let refDesc = referenda[0].description.split("\n");
 
     return (
       <div className="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
@@ -147,14 +228,203 @@ class Elections extends Component {
                   <strong>welfareiadt@gmail.com</strong>.
                 </div>
                 <div className="my-12 border-b text-center">
-                  <div className="leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2">
-                    Submit your ballot
+                  <div className="leading-none px-2 inline-block text-sm text-red-600 tracking-wide font-medium bg-white transform translate-y-1/2">
+                    IMPORTANT INFORMATION
                   </div>
                 </div>
-                <div className="mx-auto max-w-xs relative ">
-                  Sorry! Voting is not currently open. Come back on Thursday,
-                  21st January 2021!
+                <div className="mx-auto max-w-xs relative text-center font-medium text-gray-800">
+                  Hi {pref_first_name}! Welcome to IADTSU By-Elections 2021.
+                  <br />
+                  Voting is now closed. Thank you for your participation.
+                  {/* Please fill out your digital ballot carefully. Once submitted,
+                  your votes are final.
+                  <br />
+                  <br />
+                  You may vote separately for each role subject to this
+                  election, as well as any referenda. Choose your candidate for
+                  each, or vote to reopen nominations. Referenda can be voted
+                  for or against.
+                  <br />
+                  <br />
+                  To cast your vote, simply ensure that the box corresponding to
+                  your choice is selected. */}
                 </div>
+                {/* <form
+                  className="w-full flex-1 mt-8 text-indigo-500"
+                  onSubmit={this.handleSubmit}
+                >
+                  <div className="my-12 border-b text-center">
+                    <div className="leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2">
+                      Clubs & Societies Officer
+                    </div>
+                  </div>
+                  <div className="mx-auto max-w-xs relative">
+                    <div className="text-gray-800 ml-2">
+                      <div className="flex items-center mr-4 mb-4">
+                        <input
+                          id="ryan"
+                          type="radio"
+                          name="socs"
+                          value={candidates[1]._id}
+                          className="hidden"
+                          onChange={this.handleChange("socs")}
+                        />
+                        <label
+                          htmlFor="ryan"
+                          className="flex items-center cursor-pointer"
+                        >
+                          <span className="w-8 h-8 inline-block mr-2 border border-grey flex-no-shrink"></span>
+                          {candidates[1].user.pref_first_name}{" "}
+                          {candidates[1].user.last_name}
+                        </label>
+                      </div>
+
+                      <div className="flex items-center mr-4 mb-4">
+                        <input
+                          id="reopen_socs"
+                          type="radio"
+                          name="socs"
+                          value="reopen"
+                          className="hidden"
+                          onChange={this.handleChange("socs")}
+                        />
+                        <label
+                          htmlFor="reopen_socs"
+                          className="flex items-center cursor-pointer"
+                        >
+                          <span className="w-8 h-8 inline-block mr-2 border border-grey flex-no-shrink"></span>
+                          Reopen Nominations
+                        </label>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="my-12 border-b text-center">
+                    <div className="leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2">
+                      Mature Student Officer
+                    </div>
+                  </div>
+                  <div className="mx-auto max-w-xs relative">
+                    <div className="text-gray-800 ml-2">
+                      <div className="flex items-center mr-4 mb-4">
+                        <input
+                          id="neil"
+                          type="radio"
+                          name="mature"
+                          value={candidates[0]._id}
+                          className="hidden"
+                          onChange={this.handleChange("mature")}
+                        />
+                        <label
+                          htmlFor="neil"
+                          className="flex items-center cursor-pointer"
+                        >
+                          <span className="w-8 h-8 inline-block mr-2 border border-grey flex-no-shrink"></span>
+                          {candidates[0].user.pref_first_name}{" "}
+                          {candidates[0].user.last_name}
+                        </label>
+                      </div>
+
+                      <div className="flex items-center mr-4 mb-4">
+                        <input
+                          id="reopen_mature"
+                          type="radio"
+                          name="mature"
+                          value="reopen"
+                          className="hidden"
+                          onChange={this.handleChange("mature")}
+                        />
+                        <label
+                          htmlFor="reopen_mature"
+                          className="flex items-center cursor-pointer"
+                        >
+                          <span className="w-8 h-8 inline-block mr-2 border border-grey flex-no-shrink"></span>
+                          Reopen Nominations
+                        </label>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="my-12 border-b text-center">
+                    <div className="leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2">
+                      Referendum
+                    </div>
+                  </div>
+                  <div className="mx-auto max-w-xs relative">
+                    <div className="mb-4 text-gray-800 font-medium text-center">
+                      {referenda[0].title}
+                    </div>
+                    <div className="text-gray-800 mb-4">{refDesc[0]}</div>
+                    <div className="text-gray-800 ml-4 mb-1">{refDesc[1]}</div>
+                    <div className="text-gray-800 ml-4 mb-1">{refDesc[3]}</div>
+                    <div className="text-gray-800 ml-4 mb-4">{refDesc[4]}</div>
+                    <div className="text-gray-800 ml-2">
+                      <div className="flex items-center mr-4 mb-4">
+                        <input
+                          id="refFor"
+                          type="radio"
+                          name="referendum"
+                          value={true}
+                          className="hidden"
+                          onChange={this.handleChange("referendum")}
+                        />
+                        <label
+                          htmlFor="refFor"
+                          className="flex items-center cursor-pointer"
+                        >
+                          <span className="w-8 h-8 inline-block mr-2 border border-grey flex-no-shrink"></span>
+                          For
+                        </label>
+                      </div>
+
+                      <div className="flex items-center mr-4 mb-4">
+                        <input
+                          id="refAgainst"
+                          type="radio"
+                          name="referendum"
+                          value={false}
+                          className="hidden"
+                          onChange={this.handleChange("referendum")}
+                        />
+                        <label
+                          htmlFor="refAgainst"
+                          className="flex items-center cursor-pointer"
+                        >
+                          <span className="w-8 h-8 inline-block mr-2 border border-grey flex-no-shrink"></span>
+                          Against
+                        </label>
+                      </div>
+
+                      {/* <div className="flex items-center mr-4 mb-4">
+                        <input
+                          id="radio3"
+                          type="radio"
+                          name="radio"
+                          className="hidden"
+                        />
+                        <label
+                          htmlFor="radio3"
+                          className="flex items-center cursor-pointer text-xl"
+                        >
+                          <span className="w-8 h-8 inline-block mr-2 rounded-full border border-grey flex-no-shrink"></span>
+                          Third choice
+                        </label>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="my-12 border-b text-center">
+                    <div className="leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2">
+                      Submit your ballot
+                    </div>
+                  </div>
+                  <div className="mx-auto max-w-xs relative">
+                    <button
+                      onClick={this.handleSubmit}
+                      className="mt-5 tracking-wide font-semibold bg-red-600 text-gray-100 w-full py-4 rounded-lg hover:bg-red-800 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none"
+                    >
+                      <i className="fas fa-sign-out-alt  w-6  -ml-2" />
+                      <span className="ml-3">Submit Ballot</span>
+                    </button>
+                  </div>
+                </form> */}
                 <div className="my-12 border-b text-center">
                   <div className="leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2">
                     Select destination
